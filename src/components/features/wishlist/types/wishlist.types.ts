51e65ac4954c7fc89136c9ae4386e12f28e93fc0
Eugene <sifmeop@gmail.com>
2026-06-02 15:48:56 +0000

    fix types

--- src/components/features/wishlist/types/wishlist.types.ts
+++ src/components/features/wishlist/types/wishlist.types.ts
@@ -1,7 +1,7 @@
 import type { RouterOutputs } from '~/trpc/react'
 
-export type WishlistItemData = RouterOutputs['wishlist']['getAll']
+export type WishlistItemData = RouterOutputs['wishlist']['getAll'][number]
 
-export type WishlistReviewData = RouterOutputs['review']['getReviews']
+export type WishlistReviewData = RouterOutputs['review']['getReviews'][number]
 
 export type WishlistSearchStatus = 'available' | 'added' | 'reviewed'
